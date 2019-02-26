425af474acf7161a1c7157dd0e254b69953209e8
Andrew Reeman <andrewreeman@Andrews-MacBook-Air.local>
2019-02-26 19:35:52 +0000

    with passing unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?        
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My title", description: "words", image_url: "ef.jpg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 99999
+    assert product.valid?
+  end
+
+  test "image url must end in jpg, png or gif" do 
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/z/y.fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} should validate"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} should not be validate"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: "words and stuff",
+      price: 1.14,
+      image_url: "fred.gif"
+    )
+
+    assert product.invalid?
+    assert_equal [I18n.translate("errors.messages.taken")], product.errors[:title]
+    
+
+
+  end
+
+
+  def new_product(image_url)
+      Product.new(title: "tteroigre", description: "gerrt", price: 1, image_url: image_url)
+  end
 end
