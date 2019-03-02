3f64e585ccd5296dfdd72aad62a2667dd75e2424
areeman <andrewreeman@gmail.com>
2019-03-02 11:25:21 +0000

    with line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,20 @@
-class Product < ApplicationRecord
+class Product < ApplicationRecord  
+    # relations
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    def ensure_not_referenced_by_any_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, "Line Items present")
+            return false
+        end
+    end
+    
+
+    # validation
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     
@@ -12,5 +28,5 @@ class Product < ApplicationRecord
 
     def self.latest
         Product.order(:updated_at).last
-    end
+    end    
 end
