028ff8a4d9479564f6dd3a7656ecb380c3763b8b
areeman <andrewreeman@gmail.com>
2019-03-18 19:14:13 +0000

    with authentication before admin pages

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -13,4 +13,16 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to admin_url    
   end
 
+  test "should fail login" do
+    dave = users(:one)
+
+    post login_path, params: {:name => dave.name, :password => 'wrong'}
+    assert_redirected_to login_path    
+  end
+
+  test "should logout" do
+    delete logout_path
+    assert_redirected_to store_path
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,17 @@ class ActiveSupport::TestCase
   fixtures :all
   
   # Add more helper methods to be used by all tests here...
+
+  def login_as(user)
+    post login_path, params: { name: users(user).name, password: 'secret' }
+    #session[:user_id] = users(user).id
+  end
+
+  def logout
+    #session.delete :user_id
+  end
+
+  def setup    
+    login_as :one if defined? login_path
+  end
 end
