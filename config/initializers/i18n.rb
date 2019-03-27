98e9250016b2f4a2e79d415625027a3da8c8c547
areeman <andrewreeman@gmail.com>
2019-03-27 19:15:58 +0000

    with basic translations

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+
+I18n.available_locales = ['en', 'es', 'id']
+I18n.default_locale = :id
+
+LANGUAGES = [
+    ['English', 'en'],
+    ["Espa&ntilde;ol".html_safe, 'es'],
+    ['Indonesian', 'id']
+]
